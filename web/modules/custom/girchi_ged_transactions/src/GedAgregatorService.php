64918cfb085dc552b2bdd23cd932bc276f88ecd7
glontianano <46811244+glontianano@users.noreply.github.com>
2019-06-24 11:38:13 +0400

    303 transaction bug fix (#304)
    * #293 Create theme sugessiton for promo page.
    
    * #293 Create templates for promo page
    
    * #293 Paragraph templates for promo page
    
    * #293 Add markup files
    
    * #293 Alter promo page paragraphs
    
    * #293 Update config
    
    * #293 Update language
    
    * #293 Fix minor bugs
    
    * #293 Update config
    
    * #303 Get database prefix from settings.
    
    * #303 Get database prefix from settings.

--- web/modules/custom/girchi_ged_transactions/src/GedAgregatorService.php
+++ web/modules/custom/girchi_ged_transactions/src/GedAgregatorService.php
@@ -2,14 +2,14 @@
 
 namespace Drupal\girchi_ged_transactions;
 
-
 class GedAgregatorService
 {
   public function calculateAndUpdateTotalGeds($uid)
   {
     $connection = \Drupal::database();
+    $prefix = $connection->tablePrefix();
     $query = $connection->query(
-      "SELECT SUM(ged_amount) AS `ged_amount` FROM `girchidrpl_ged_transaction_field_data` WHERE `user` = :id",
+      "SELECT SUM(ged_amount) AS `ged_amount` FROM `{$prefix}ged_transaction_field_data` WHERE `user` = :id",
       [
         ':id' => $uid,
       ]
