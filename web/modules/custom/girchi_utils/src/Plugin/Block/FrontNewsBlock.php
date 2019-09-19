9ac4eb264b736ec54a09e7a87fc1f609b7ade1db
glontianano <glonti1899@gmail.com>
2019-09-19 17:58:32 +0400

    #486 Sort news with published date.

--- web/modules/custom/girchi_utils/src/Plugin/Block/FrontNewsBlock.php
+++ web/modules/custom/girchi_utils/src/Plugin/Block/FrontNewsBlock.php
@@ -4,8 +4,10 @@ namespace Drupal\girchi_utils\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\Cache;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\node\Entity\Node;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'Front news block' block.
@@ -15,19 +17,58 @@ use Drupal\node\Entity\Node;
  *  admin_label = @Translation("Front news block"),
  * )
  */
-class FrontNewsBlock extends BlockBase {
+class FrontNewsBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  /**
+   * Entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * FrontNewsBlock constructor.
+   *
+   * @param array $configuration
+   *   Array of configuration.
+   * @param int $plugin_id
+   *   Plugin id.
+   * @param string $plugin_definition
+   *   Plugin definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function blockForm($form, FormStateInterface $form_state) {
     $vid = 'news_categories';
-    $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid);
+    $terms = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($vid);
     $term_data = [];
     foreach ($terms as $term) {
       $term_data[$term->tid] = $term->name;
     }
-    $term_data = ['all' => t('All')] + $term_data;
+    $term_data = ['all' => $this->t('All')] + $term_data;
     $form['category_select'] = [
       '#type' => 'select',
       '#title' => $this->t('Select category'),
@@ -49,16 +90,17 @@ class FrontNewsBlock extends BlockBase {
    */
   public function build() {
     $category_id = $this->configuration['category_select'];
-    $em = \Drupal::entityTypeManager();
+    $em = $this->entityTypeManager;
 
-    /** @var \Drupal\node\Entity\NodeStorage $node_storage */
+    /** @var \Drupal\node\NodeStorage $node_storage */
     $node_storage = $em->getStorage('node');
     if ($category_id == 'all') {
       $lastest_articles = $node_storage->getQuery()
         ->condition('type', 'article')
         ->condition('status', 1)
         ->condition('field_is_video', '0')
-        ->sort('created', "DESC")
+        ->sort('field_published_date', 'DESC')
+        ->sort('created', 'DESC')
         ->range(0, 10)
         ->execute();
     }
@@ -71,16 +113,14 @@ class FrontNewsBlock extends BlockBase {
         ->range(0, 10)
         ->execute();
     }
-
-    $articles = Node::loadMultiple($lastest_articles);
-    krsort($articles);
+    $articles = $node_storage->loadMultiple($lastest_articles);
 
     $template = [
       '#theme' => 'front_page_articles',
       '#articles' => $articles,
     ];
 
-    $category = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($category_id);
+    $category = $em->getStorage('taxonomy_term')->load($category_id);
     if ($category) {
       $template['#category'] = $category->getName();
     }
