a9a88ee277b74f5f2cc75bc50b62089e75b16eca
glontianano <glonti1899@gmail.com>
2019-10-09 15:59:24 +0400

    #506 Fix validation bug.

--- web/modules/custom/girchi_utils/src/Validate/ValidationConstraint.php
+++ web/modules/custom/girchi_utils/src/Validate/ValidationConstraint.php
@@ -17,7 +17,7 @@ class ValidationConstraint {
       $webform_field = ($form['elements']['investment_amount']);
       $is_parent = $formState->getValue('i_am_a_parent');
       $investment_amount = $formState->getValue('investment_amount');
-      if ($is_parent === TRUE && $investment_amount < 10000) {
+      if ($is_parent == TRUE && $investment_amount < 10000) {
         $formState->setError($webform_field, t('Minimum amount of investment for parents must be equal to 10K dollar.'));
       }
     }
