a0c2d2c2a9179bfdbb1b17a5004fe45931ba1f4c
GGKapanadze <kapanadzeg41@gmail.com>
2019-10-28 16:08:53 +0400

    Add first donation to relataed donations.

--- web/modules/custom/girchi_donations/src/Controller/DonationsController.php
+++ web/modules/custom/girchi_donations/src/Controller/DonationsController.php
@@ -242,9 +242,6 @@ class DonationsController extends ControllerBase {
           if ($reg_donation->getStatus() !== 'ACTIVE') {
             /** @var \Drupal\user\Entity\User $user */
             $reg_donation->setStatus('ACTIVE');
-            $reg_donation->save();
-            $this->getLogger('girchi_donations')
-              ->info('Regular donation was activated.');
             $ged_amount = $this->gedCalculator->calculate($reg_donation->get('amount')->value);
             $transaction = $ged_manager->create([
               'user_id' => "1",
@@ -260,7 +257,7 @@ class DonationsController extends ControllerBase {
               ->info("Ged transaction was made.");
             $type = $reg_donation->get('type')->value;
             $entity_id = $reg_donation->get('aim_id')->target_id ? $reg_donation->get('aim_id')->target_id : $reg_donation->get('politician_id')->target_id;
-            $this->donationUtils->addDonationRecord($type, [
+            $donation = $this->donationUtils->addDonationRecord($type, [
               'trans_id' => $reg_donation->get('trans_id')->value,
               'amount' => (int) $reg_donation->get('amount')->value,
               'user_id' => $reg_donation->getOwnerId(),
@@ -268,7 +265,13 @@ class DonationsController extends ControllerBase {
               'field_ged_transaction' => $transaction->id(),
               'status' => 'OK',
             ], $entity_id);
+            /** @var \Drupal\Core\Field\EntityReferenceFieldItemList $donations */
+            $donations = $reg_donation->get('field_donations');
+            $donations->appendItem($donation->id());
             $this->getLogger('girchi_donations')->info("Donation was made.");
+            $reg_donation->save();
+            $this->getLogger('girchi_donations')
+              ->info('Regular donation was activated.');
             $reg_donation_details = [
               'frequency' => $reg_donation->get('frequency')->value,
               'day' => $reg_donation->get('payment_day')->value,
