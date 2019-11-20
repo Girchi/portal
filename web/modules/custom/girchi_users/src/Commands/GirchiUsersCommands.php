a9b17ad63b498be1b4fd92e5cdaae135b6db095d
glontianano <glonti1899@gmail.com>
2019-11-20 17:05:06 +0400

    Create drush command for old numbers.

---
+++ web/modules/custom/girchi_users/src/Commands/GirchiUsersCommands.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\girchi_users\Commands;
+
+use Drupal\Core\Entity\EntityTypeManager;
+use Drush\Commands\DrushCommands;
+
+/**
+ * A Drush commandfile.
+ *
+ * In addition to this file, you need a drush.services.yml
+ * in root of your module, and a composer.json file that provides the name
+ * of the services file to use.
+ *
+ * See these files for an example of injecting Drupal services:
+ *   - http://cgit.drupalcode.org/devel/tree/src/Commands/DevelCommands.php
+ *   - http://cgit.drupalcode.org/devel/tree/drush.services.yml
+ */
+class GirchiUsersCommands extends DrushCommands {
+  /**
+   * EntityTypeManagerInterface definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * EntityTypeManager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  public function __construct(EntityTypeManager $entityTypeManager) {
+    parent::__construct();
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * Main command.
+   *
+   * @command girchi_users:fix-field-tel
+   * @aliases fix-field-tel
+   */
+  public function fixFieldTel() {
+    $user_storage = $this->entityTypeManager->getStorage('user');
+    $users_id = $user_storage->getQuery()
+      ->condition('field_phone', NULL, 'IS NOT NULL')
+      ->execute();
+    $users = $user_storage->loadMultiple($users_id);
+    foreach ($users as $user) {
+      $old_value = $user->get('field_phone')->value;
+      $user->get('field_tel')->value = NULL == $user->get('field_tel')->value ? $old_value : $user->get('field_tel')->value;
+      $user->save();
+    }
+  }
+
+}
