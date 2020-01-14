5c6f85e9a679c34b15fe469cd28f086164d8ae79
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-01-14 13:06:41 +0400

    #594 Add Paypal JS sdk to libraries.

---
+++ web/themes/custom/girchi/js/paypal.js
@@ -0,0 +1,64 @@
+$("document").ready(function () {
+    paypal.Buttons({
+        style: {
+            layout: 'vertical',
+            color: 'gold',
+            shape: 'rect',
+            label: 'paypal',
+        },
+        onInit: function (data, actions) {
+
+            // Disable the buttons
+            actions.disable();
+
+            // Listen for changes to the input
+            document.querySelector('#single-donation form')
+                .addEventListener('change', function (event) {
+                    // Enable or disable the button when it is checked or unchecked
+                    if ($('#edit-amount').val() != '') {
+                        if ($("#edit-politicians option:selected").val() != ''
+                            || $("#edit-donation-aim option:selected").val() != '') {
+                            actions.enable();
+                        } else {
+                            actions.disable();
+                        }
+                    } else {
+                        actions.disable();
+                    }
+                });
+        },
+
+        createOrder: function (data, actions) {
+            var amount = $("#edit-amount").val();
+            // This function sets up the details of the transaction
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: amount
+                    }
+                }]
+            });
+        },
+        onApprove: function (data, actions) {
+            var aim = $("#edit-donation-aim option:selected").val();
+            var politician = $("#edit-politicians option:selected").val();
+
+            return actions.order.capture().then(function (details) {
+                console.log(data);
+                // Call server to save the transaction
+                window.location.replace("/donate/finish");
+                return fetch('/donate/finish/paypal', {
+                    method: 'post',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        order_id: data.orderID,
+                        aim: aim,
+                        politician: politician
+                    })
+                });
+            });
+        }
+    }).render('#paypal-button-container');
+});
