1a6460671dd81de839cf67a3b1f8cd945ab911bb
glontianano <glonti1899@gmail.com>
2020-01-15 21:24:36 +0400

    Add system message.

--- web/themes/custom/girchi/js/global.js
+++ web/themes/custom/girchi/js/global.js
@@ -105,6 +105,12 @@ $(document).ready(function() {
         $('#referralModal').modal('show');
     }
 
+    if(window.location.search == '?pass-reset=success'){
+        console.log(jQuery("form"));
+        $("#user-login-form").prepend(`<div class="alert alert-success">${Drupal.t("Your password has been successfully changed. Please log in into your account with new password.")} </div>`);
+
+    };
+
 });
 
 function SetCaretAtEnd(elem) {
