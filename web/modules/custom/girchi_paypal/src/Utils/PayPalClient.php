470271772f7ceb019b91e8fe0cf2b71e0651361b
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-01-17 18:28:20 +0400

    #594 Remove error showing from Paypal client.

--- web/modules/custom/girchi_paypal/src/Utils/PayPalClient.php
+++ web/modules/custom/girchi_paypal/src/Utils/PayPalClient.php
@@ -7,11 +7,6 @@ use PayPalCheckoutSdk\Core\ProductionEnvironment;
 use PayPalCheckoutSdk\Core\SandboxEnvironment;
 use Symfony\Component\Dotenv\Dotenv;
 
-// Or error_reporting(E_ALL);
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-
 /**
  * Client for paypal api.
  */
