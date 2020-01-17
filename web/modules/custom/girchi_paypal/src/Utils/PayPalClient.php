869ded6d233614bcef162444217a8493740a07e3
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-01-17 12:04:04 +0400

    #594 Create seperate env file for credentials.

--- web/modules/custom/girchi_paypal/src/Utils/PayPalClient.php
+++ web/modules/custom/girchi_paypal/src/Utils/PayPalClient.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace Drupal\girchi_paypal\Controller;
+namespace Drupal\girchi_paypal\Utils;
 
 use PayPalCheckoutSdk\Core\PayPalHttpClient;
+use PayPalCheckoutSdk\Core\ProductionEnvironment;
 use PayPalCheckoutSdk\Core\SandboxEnvironment;
 use Symfony\Component\Dotenv\Dotenv;
 
@@ -27,13 +28,20 @@ class PayPalClient {
    * Setting PayPal SDK environment.
    */
   public static function environment() {
-    // Load certificate path and pass.
+    $moduleHandler = \Drupal::service('module_handler');
+    $modulePath = $moduleHandler->getModule('girchi_paypal')->getPath();
 
+    // Load Paypal clinet id and secret pass.
     $dotEnv = new Dotenv();
-    $dotEnv->load($modulePath . '/cert/.cert.env');
-    $clientId = getenv("CLIENT_ID") ?: "AUCemHy3DQN_XhP9dqNahbnOwM2kawkNr-shr7KIED1F3cGDObPi2Iw2UhTUPMSKmnT6e_1i7C9HhyJ4";
-    $clientSecret = getenv("CLIENT_SECRET") ?: "EIZhdPEehTXNMpNBT7r2ZTeF-LhWBSapuJ8Zg4ynN3xkRa8M15_h2GLWX6iVuXKnzLDIJwb7V6hYoV5e";
-    return new SandboxEnvironment($clientId, $clientSecret);
+    $dotEnv->load($modulePath . '/credentials/.credentials.env');
+    $clientId = getenv("CLIENT_ID") ?: $_ENV['CLIENT_ID'];
+    $clientSecret = getenv("CLIENT_SECRET") ?: $_ENV['CLIENT_SECRET'];
+    if ($_ENV['ENV'] == 'production') {
+      return new ProductionEnvironment($clientId, $clientSecret);
+    }
+    else {
+      return new SandboxEnvironment($clientId, $clientSecret);
+    }
   }
 
 }
