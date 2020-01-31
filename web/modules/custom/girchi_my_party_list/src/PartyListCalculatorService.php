b576929461dec86b3f2906ebc2000387a95e429e
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-01-31 12:28:20 +0400

    Refactor party list calculation function.

--- web/modules/custom/girchi_my_party_list/src/PartyListCalculatorService.php
+++ web/modules/custom/girchi_my_party_list/src/PartyListCalculatorService.php
@@ -2,10 +2,9 @@
 
 namespace Drupal\girchi_my_party_list;
 
-use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
-use Drupal\Component\Plugin\Exception\PluginNotFoundException;
-use Drupal\Core\Entity\EntityStorageException;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Logger\LoggerChannelFactory;
 use Drupal\image\Entity\ImageStyle;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -27,6 +26,18 @@ class PartyListCalculatorService {
    * @var \Symfony\Component\HttpFoundation\RequestStack
    */
   protected $requestStack;
+  /**
+   * Database.
+   *
+   * @var \Drupal\Core\Database\Database
+   */
+  private $database;
+  /**
+   * LoggerFactory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactory
+   */
+  private $loggerFactory;
 
   /**
    * Constructs a new PartyListCalculatorService object.
@@ -37,10 +48,19 @@ class PartyListCalculatorService {
    * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
    *
    *   Request stack;.
+   * @param \Drupal\Core\Logger\LoggerChannelFactory $loggerFactory
+   *   LoggerFactory.
+   * @param \Drupal\Core\Database\Connection $database
+   *   Database.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, RequestStack $requestStack) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager,
+                              RequestStack $requestStack,
+                              LoggerChannelFactory $loggerFactory,
+                              Connection $database) {
     $this->entityTypeManager = $entity_type_manager;
     $this->requestStack = $requestStack->getCurrentRequest();
+    $this->loggerFactory = $loggerFactory->get('girchi_party_list');
+    $this->database = $database;
   }
 
   /**
@@ -54,75 +74,57 @@ class PartyListCalculatorService {
       /**
        * @var \Drupal\user\Entity\UserStorage $users
        */
-      $user_storage = $this->entityTypeManager->getStorage('user');
-      $user_ids = $user_storage->getQuery()
-        ->condition('field_ged', '0', '>')
-        ->condition('field_my_party_list', '0', '>')
-        ->execute();
-      $politicians = $user_storage
-        ->getQuery()
-        ->condition('field_politician', TRUE, '=')
-        ->execute();
-      $users = $user_storage->loadMultiple($user_ids);
-      /**
-       * @var \Drupal\user\Entity\User $user
-       */
-      if (!empty($users)) {
-        foreach ($users as $user) {
-
-          $user_party_list = $user->get('field_my_party_list')->getValue();
-          $user_ged = (int) $user->get('field_ged')->getValue()[0]['value'];
-          foreach ($user_party_list as $party_list_item) {
-            $percentage = (int) $party_list_item['value'];
-            if ($percentage > 100) {
-              $percentage = 100;
-            }
-            elseif ($percentage < 0) {
-              $percentage = 0;
-            }
-            $uid = $party_list_item['target_id'];
-            unset($politicians[$uid]);
-            if (isset($user_rating[$uid])) {
-              $user_rating[$uid] += $user_ged * ($percentage / 100);
-            }
-            else {
-              $user_rating[$uid] = $user_ged * ($percentage / 100);
-            }
-          };
+      $user_storage = $this->entityTypeManager
+        ->getStorage('user');
+      $query = $this->database->select('user__field_my_party_list', 'pr');
+      $query->leftJoin('user__field_ged', 'ged', 'pr.entity_id = ged.entity_id');
+      $query->leftJoin('user__field_first_name', 'fn', 'pr.entity_id = fn.entity_id');
+      $query->leftJoin('user__field_last_name', 'ln', 'pr.entity_id = ln.entity_id');
+      $query
+        ->fields('pr', ['entity_id',
+          'field_my_party_list_target_id',
+          'field_my_party_list_value',
+        ])
+        ->fields('ged', ['field_ged_value'])
+        ->condition('pr.field_my_party_list_target_id', NULL, 'IS NOT NULL')
+        ->condition('pr.field_my_party_list_value', NULL, 'IS NOT NULL')
+        ->condition('fn.field_first_name_value', NULL, 'IS NOT NULL')
+        ->condition('ln.field_last_name_value', NULL, 'IS NOT NULL');
+      $results = $query->execute()->fetchAll();
+      foreach ($results as $result) {
+        $id = $result->field_my_party_list_target_id;
+        $percent = $result->field_my_party_list_value;
+        $ged = $result->field_ged_value;
+        if (isset($user_rating[$id])) {
+          $user_rating[$id] += $ged * ($percent / 100);
         }
-        $this->cleanUpPartyList($politicians);
-        arsort($user_rating);
+        else {
+          $user_rating[$id] = $ged * ($percent / 100);
+        }
+      }
+      arsort($user_rating);
 
-        $rating_number = 1;
-        foreach ($user_rating as $uid => $ged_amount) {
+      $rating_number = 1;
+      $politicians = $user_storage->loadMultiple(array_keys($user_rating));
+
+      foreach ($politicians as $politician) {
+        try {
           /**
            * @var \Drupal\user\Entity\User $politician
            */
-          $politician = $user_storage->load($uid);
-
-          if (!in_array($uid, $politicians) && $politician != NULL) {
-            if ($politician->get('field_politician')->value == TRUE) {
-              $politician->set('field_rating_in_party_list', $rating_number);
-              $politician->set('field_political_ged', $ged_amount);
-              try {
-                $politician->save();
-                $rating_number++;
-              }
-              catch (EntityStorageException $e) {
-                $this->loggerFactory->error($e->getMessage());
-              }
-            }
-          }
+          $politician->set('field_political_ged', $user_rating[$politician->id()]);
+          $politician->set('field_rating_in_party_list', $rating_number);
+          $politician->save();
+          $rating_number++;
+        }
+        catch (\Exception $e) {
+          $this->loggerFactory->error($e->getMessage());
         }
       }
     }
-    catch (InvalidPluginDefinitionException $e) {
+    catch (\Exception $e) {
       $this->loggerFactory->error($e->getMessage());
     }
-    catch (PluginNotFoundException $e) {
-      $this->loggerFactory->error($e->getMessage());
-    }
-
   }
 
   /**
@@ -189,28 +191,6 @@ class PartyListCalculatorService {
       });
     }
     return $returnArray;
-  }
-
-  /**
-   * Clean up party list.
-   *
-   * @param array $politicians
-   *   Politician uids.
-   *
-   * @throws \Drupal\Core\Entity\EntityStorageException
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   */
-  private function cleanUpPartyList(array $politicians) {
-    $user_storage = $this->entityTypeManager->getStorage('user');
-    $users = $user_storage->loadMultiple($politicians);
-    if (!empty($users)) {
-      foreach ($users as $user) {
-        $user->set('field_political_ged', 0);
-        $user->set('field_rating_in_party_list', NULL);
-        $user->save();
-      }
-    }
   }
 
 }
