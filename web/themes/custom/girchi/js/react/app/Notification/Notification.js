8ee20e31a92fd4729e619dc5a5ad7e2df7c4060c
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-03-31 12:10:29 +0400

    Create notification component.

---
+++ web/themes/custom/girchi/js/react/app/Notification/Notification.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import "./Notification.css";
+
+const Notification = ({ notification, readNotification }) => {
+    const { _id, title, desc, link, photoUrl, created, isRead } = notification;
+    const [read, setRead] = useState(isRead);
+
+    useEffect(() => {}, [read, setRead]);
+
+    return (
+        <div className="notifications__notifi-box__item">
+            <a
+                className={
+                    !read ? "notifications-unread" : "notifications-seen"
+                }
+                href={link}
+                onClick={e => {
+                    e.preventDefault();
+                    readNotification(_id);
+                    if (!read) {
+                        setRead(true);
+                    }
+                }}
+            >
+                <div className="notifications__notifi-box__item__img">
+                    <img src={photoUrl} />
+                </div>
+                <div className="notifications__notifi-box__item__text">
+                    <h4>{title}</h4>
+                    <p>{desc}</p>
+                    <span className="notify-time">{created}</span>
+                </div>
+                <div className="notifications__notifi-box__item__corner-circle active">
+                    <div></div>
+                </div>
+            </a>
+        </div>
+    );
+};
+
+export default Notification;
