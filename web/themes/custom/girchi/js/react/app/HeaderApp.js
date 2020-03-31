f7c0dd4b0c730abdb05fa484bea94d4a1a6d4fb6
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-03-31 12:11:40 +0400

    Create App component for header.

---
+++ web/themes/custom/girchi/js/react/app/HeaderApp.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState, useRef } from "react";
+import Axios from "axios";
+import NotificationWrapper from "./NotificationWrapper/NotificationWrapper";
+import AppContext from "./AppContext";
+
+const HeaderApp = ({ accessToken, refreshToken, socket }) => {
+    const [notifications, setNotifications] = useState([]);
+    const appHook = useState({
+        unreadCount: 0
+    });
+
+    const getNotifications = () => {
+        Axios.get(
+            "http://notifications.girchi.docker.localhost/notifications/user",
+            {
+                withCredentials: true
+            }
+        ).then(
+            res => {
+                setNotifications(res.data.notifications);
+            },
+            err => console.log(err)
+        );
+    };
+
+    useEffect(() => {
+        getNotifications();
+        socket.on("notification added", notification => {
+            setNotifications(currentNotifications => [
+                notification,
+                ...currentNotifications
+            ]);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (notifications.length > 4) {
+            notifications.pop();
+        }
+    }, [notifications]);
+
+    return (
+        <AppContext.Provider value={appHook}>
+            <NotificationWrapper notifications={notifications} />
+        </AppContext.Provider>
+    );
+};
+
+export default HeaderApp;
