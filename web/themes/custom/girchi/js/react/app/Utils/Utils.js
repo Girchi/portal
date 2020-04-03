1f414080040a02a0cdf165d28ebaa59adcc25ea2
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-04-03 17:03:27 +0400

    Regenerate access token if expired and return it.

--- web/themes/custom/girchi/js/react/app/Utils/Utils.js
+++ web/themes/custom/girchi/js/react/app/Utils/Utils.js
@@ -1,15 +1,17 @@
 import jwt from "jsonwebtoken";
 import Axios from "axios";
+import Cookies from "js-cookie";
 
-export const generateJwtIfExpired = accessToken => {
+export const generateJwtIfExpired = () => {
+    let accessToken = Cookies.get("g-u-at");
     if (accessToken) {
         const { exp } = jwt.decode(accessToken);
         if (new Date().getTime() > exp * 1000) {
             Axios.get("/api/jwt/token_refresh").then(
-                res => console.log(res),
+                res => (accessToken = Cookies.get("g-u-at")),
                 error => console.log(error)
             );
-        } else {
         }
+        return accessToken;
     }
 };
