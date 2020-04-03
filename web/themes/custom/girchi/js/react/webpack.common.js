42d45ebd08ef6a25e60c2d141238528bb1a8ba28
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-04-03 17:01:07 +0400

    Create .env for app.

--- web/themes/custom/girchi/js/react/webpack.common.js
+++ web/themes/custom/girchi/js/react/webpack.common.js
@@ -1,5 +1,16 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+
+// call dotenv and it will return an Object with a parsed key
+const env = dotenv.config().parsed;
+
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`${next}`] = JSON.stringify(env[next]);
+    return prev;
+}, {});
 
 module.exports = {
     entry: "./app/index.js",
@@ -30,5 +41,10 @@ module.exports = {
             }
         ]
     },
-    plugins: [new CleanWebpackPlugin()]
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({
+            "process.env": envKeys
+        })
+    ]
 };
