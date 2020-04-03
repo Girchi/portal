160d0ea1501c2cd0e6848c0d634a09ff9b28ca1a
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-04-03 17:05:52 +0400

    Rewrite AppContext.

--- web/themes/custom/girchi/js/react/app/AppContext.js
+++ web/themes/custom/girchi/js/react/app/AppContext.js
@@ -1,7 +1,33 @@
-import { createContext } from "react";
+import React, { createContext, useState } from "react";
 
-const AppContext = createContext({
+const initialState = {
     unreadCount: 0
-});
+};
 
-export default AppContext;
+const AppContext = createContext(initialState);
+let reducer = (state, action) => {
+    switch (action.type) {
+        case "reset":
+            return initialState;
+        case "increment":
+            return { ...state, unreadCount: state.unreadCount + 1 };
+        case "decrement":
+            return { ...state, unreadCount: state.unreadCount - 1 };
+        case "setUnreadCount":
+            return { ...state, unreadCount: action.payload };
+    }
+};
+
+function AppContextProvider(props) {
+    let [state, dispatch] = React.useReducer(reducer, initialState);
+    let value = { state, dispatch };
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    );
+}
+const AppContextConsumer = AppContext.Consumer;
+
+export { AppContext, AppContextProvider, AppContextConsumer };
