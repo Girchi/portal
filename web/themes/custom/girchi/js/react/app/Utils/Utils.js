328d86c13c209b7d060b9725bd295c7e81a8ddec
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-04-07 18:17:42 +0400

    Fix util function to set new access token in cookies.

--- web/themes/custom/girchi/js/react/app/Utils/Utils.js
+++ web/themes/custom/girchi/js/react/app/Utils/Utils.js
@@ -2,16 +2,32 @@ import jwt from "jsonwebtoken";
 import Axios from "axios";
 import Cookies from "js-cookie";
 
-export const generateJwtIfExpired = () => {
+export async function generateJwtIfExpired() {
     let accessToken = Cookies.get("g-u-at");
     if (accessToken) {
         const { exp } = jwt.decode(accessToken);
         if (new Date().getTime() > exp * 1000) {
-            Axios.get("/api/jwt/token_refresh").then(
-                res => (accessToken = Cookies.get("g-u-at")),
-                error => console.log(error)
-            );
+            await Axios.get("/api/jwt/token_refresh", {
+                withCredentials: true
+            }).then(res => {
+                if (res.data.status == "success") {
+                    accessToken = res.data.tokens.accessToken;
+                    const refreshToken = res.data.tokens.refreshToken;
+                    const in30Minutes = 1 / 48;
+                    const domain = `.${window.location.hostname}`;
+                    Cookies.set("g-u-at", accessToken, {
+                        expires: in30Minutes,
+                        domain: ".girchi.docker.localhost"
+                    });
+                    Cookies.set("g-u-rt", refreshToken, {
+                        expires: in30Minutes,
+                        domain: ".girchi.docker.localhost"
+                    });
+                } else {
+                    console.log(res);
+                }
+            });
         }
-        return accessToken;
     }
-};
+    return accessToken;
+}
--- web/themes/custom/girchi/js/react/app/index.js
+++ web/themes/custom/girchi/js/react/app/index.js
@@ -8,34 +8,32 @@ import io from "socket.io-client";
 import { AppContextProvider } from "./AppContext";
 
 const ENDPOINT = process.env.REACT_APP_ENDPOINT;
-
-const accessToken = generateJwtIfExpired();
-if (accessToken) {
-    let socket = io(ENDPOINT, { transports: ["websocket"] });
-
-    socket.emit("auth", { accessToken }, err => {
-        console.log(err);
-    });
-
-    ReactDOM.render(
-        <AppContextProvider>
-            <HeaderApp accessToken={accessToken} socket={socket} />
-        </AppContextProvider>,
-        document.getElementById("notifications-header")
-    );
-    ReactDOM.render(
-        <AppContextProvider>
-            <HeaderApp accessToken={accessToken} socket={socket} />
-        </AppContextProvider>,
-        document.getElementById("notifications-sticky")
-    );
-    const element = document.getElementById("notifications");
-    if (typeof element != "undefined" && element != null) {
+generateJwtIfExpired().then(accessToken => {
+    if (accessToken) {
+        let socket = io(ENDPOINT, { transports: ["websocket"] });
+        socket.emit("auth", { accessToken }, err => {
+            console.log(err);
+        });
         ReactDOM.render(
             <AppContextProvider>
-                <App socket={socket} accessToken={accessToken} />
+                <HeaderApp accessToken={accessToken} socket={socket} />
             </AppContextProvider>,
-            element
+            document.getElementById("notifications-header")
         );
+        ReactDOM.render(
+            <AppContextProvider>
+                <HeaderApp accessToken={accessToken} socket={socket} />
+            </AppContextProvider>,
+            document.getElementById("notifications-sticky")
+        );
+        const element = document.getElementById("notifications");
+        if (typeof element != "undefined" && element != null) {
+            ReactDOM.render(
+                <AppContextProvider>
+                    <App socket={socket} accessToken={accessToken} />
+                </AppContextProvider>,
+                element
+            );
+        }
     }
-}
+});
