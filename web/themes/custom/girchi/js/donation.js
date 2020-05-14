c786d2c635ef7bb0ce83c8087c76f7be560ce959
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-05-14 17:13:25 +0400

    Fix whitespace.

--- web/themes/custom/girchi/js/donation.js
+++ web/themes/custom/girchi/js/donation.js
@@ -33,7 +33,7 @@ $("document").ready(function () {
             $(selectedOption).attr('selected','selected');
             $(selectEl).selectpicker('refresh');
             $(`#del-sel-option-${sourceAttr}`).removeClass('d-none');
-	     $(`.${sourceAttr}-hidden-politician`).val(politician_id);
+	        $(`.${sourceAttr}-hidden-politician`).val(politician_id);
         }
 
         selectEl.on('changed.bs.select',function(e){
