73d1c42d07c9bf2a78cb18a7acd5ac9226e2c688
glontianano <glonti1899@gmail.com>
2020-05-19 15:56:56 +0400

    Add badge constants.

---
+++ web/modules/custom/girchi_users/src/Constants/BadgeConstants.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Drupal\girchi_users\Constants;
+
+/**
+ * Class BadgeConstants.
+ */
+class BadgeConstants
+{
+  const POLITICIAN = 'politician';
+  const PORTAL_MEMBER = 'Member of the portal';
+  const REGULAR_CONTRIBUTOR = 'პარტნიორი - მრავალჯერადი დამფინანსებელი';
+  const SINGLE_CONTRIBUTOR = 'პარტნიორი - ერთჯერადი დამფინანსებელი';
+
+
+
+}
