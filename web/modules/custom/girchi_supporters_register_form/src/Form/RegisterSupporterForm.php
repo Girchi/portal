ea484399920d60feec64f893ec04cbd85e928daf
Giorgi Jibladze <giorgi@omedia.ge>
2020-07-05 02:00:03 +0400

    Add basic validations for personal id, email and phone number fields. refs #706

--- web/modules/custom/girchi_supporters_register_form/src/Form/RegisterSupporterForm.php
+++ web/modules/custom/girchi_supporters_register_form/src/Form/RegisterSupporterForm.php
@@ -19,11 +19,19 @@ class RegisterSupporterForm extends FormBase {
   protected $currentUser;
 
   /**
+   * UserUtils service.
+   *
+   * @var Drupal\girchi_users\UsersUtils
+   */
+  protected $usersUtils;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
     $instance = parent::create($container);
     $instance->currentUser = $container->get('current_user');
+    $instance->usersUtils = $container->get('girchi_users.utils');
     return $instance;
   }
 
@@ -38,41 +46,56 @@ class RegisterSupporterForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['government_id'] = [
+    $form['registrator'] = [
+      '#markup' => '<div class="registrator-box">Registrator: <strong>' . $this->currentUser->getDisplayName() . '</strong></div>',
+    ];
+    $form['gov_id'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Government ID'),
+      '#title' => 'Government ID',
       '#maxlength' => 11,
       '#size' => 11,
       '#weight' => '0',
+      '#required' => 1,
     ];
     $form['firstname'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Firstname'),
+      '#title' => 'Firstname',
       '#maxlength' => 64,
       '#size' => 64,
       '#weight' => '1',
+      '#required' => 1,
     ];
     $form['lastname'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Lastname'),
+      '#title' => 'Lastname',
       '#maxlength' => 64,
       '#size' => 64,
       '#weight' => '2',
+      '#required' => 1,
     ];
     $form['email'] = [
       '#type' => 'email',
-      '#title' => $this->t('Email'),
+      '#title' => 'Email',
       '#weight' => '3',
+      '#required' => 1,
     ];
     $form['phone'] = [
       '#type' => 'tel',
-      '#title' => $this->t('Phone'),
+      '#title' => 'Phone',
       '#weight' => '4',
+      '#required' => 1,
+      '#default_value' => '+995',
+    ];
+    $form['ged_amount'] = [
+      '#type' => 'number',
+      '#title' => 'GED amount',
+      '#weight' => '5',
+      '#default' => 0,
     ];
     $form['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Register'),
-      '#weight' => '5',
+      '#value' => 'Register',
+      '#weight' => '100',
     ];
 
     return $form;
@@ -81,7 +104,26 @@ class RegisterSupporterForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+    $values = $form_state->getValues();
+
+    if ($this->usersUtils->fieldIsTaken('field_personal_id', $values['gov_id'])) {
+      $form_state->setErrorByName('gov_id', sprintf('Personal number %s is already used by other girchi.com user.', $values['gov_id']));
+    }
+
+    if ($this->usersUtils->fieldIsTaken('mail', $values['email'])) {
+      $form_state->setErrorByName('email', sprintf('Email %s is already used by other girchi.com user.', $values['email']));
+    }
+
+    if ($this->usersUtils->fieldIsTaken('field_tel', $values['phone'])) {
+      $form_state->setErrorByName('phone', sprintf('Phone number %s is already used by other girchi.com user.', $values['phone']));
+    }
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {}
 
 }
