d53b34b18705a3e447816be73bf37b8e4ea390cc
Ilia Nozadze <i_nozadze@cu.edu.ge>
2020-11-24 16:52:15 +0400

    Optimize total ged calculation.

--- web/modules/custom/girchi_ged_transactions/src/SummaryGedCalculationService.php
+++ web/modules/custom/girchi_ged_transactions/src/SummaryGedCalculationService.php
@@ -4,6 +4,7 @@ namespace Drupal\girchi_ged_transactions;
 
 use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Logger\LoggerChannelFactory;
 
@@ -27,39 +28,47 @@ class SummaryGedCalculationService {
   protected $loggerFactory;
 
   /**
+   * Database.
+   *
+   * @var \Drupal\Core\Database\Database
+   */
+  protected $database;
+
+  /**
    * Constructs a new SummaryGedCalculationService object.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   Entity type manager.
    * @param \Drupal\Core\Logger\LoggerChannelFactory $loggerFactory
    *   Logger messages.
+   * @param \Drupal\Core\Database\Connection $database
+   *   Database.
    */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager, LoggerChannelFactory $loggerFactory) {
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, LoggerChannelFactory $loggerFactory, Connection $database) {
     $this->entityTypeManager = $entity_type_manager;
     $this->loggerFactory = $loggerFactory->get('girchi_ged_transactions');
+    $this->database = $database;
   }
 
   /**
    * SummaryGedCalculation.
    */
   public function summaryGedCalculation() {
-    $users = NULL;
     $gedValue = 0;
+    $gedPercentage = 0;
     $arr = [];
     try {
-      $user_storage = $this->entityTypeManager->getStorage('user');
-      $user_ids = $user_storage->getQuery()
-        ->condition('field_ged', '0', '>')
-        ->execute();
-      /** @var \Drupal\user\Entity\User $users */
-      $users = $user_storage->loadMultiple($user_ids);
 
-      foreach ($users as $user) {
-        $gedArray = $user->get('field_ged')->getValue();
-        $gedValue = $gedValue + (int) $gedArray[0]['value'];
+      $this->database->query("SET SQL_MODE=''");
+      $sth = $this->database->select('user__field_ged', 'field_ged')
+        ->fields('field_ged', ['field_ged_value']);
+
+      $sth->addExpression('sum(field_ged.field_ged_value)', 'total_count');
+      $data = $sth->execute()->fetchAll();
+      if (isset($data[0]->total_count)) {
+        $gedValue = $data[0]->total_count;
+        $gedPercentage = $gedValue / 50000000;
       }
-
-      $gedPercentage = $gedValue / 50000000;
 
       $arr = [
         'gedValue' => $gedValue,
